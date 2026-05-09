7b3044f0fdaad7229808507539c802ca4071e41c
Deepti-web <deeptinirmalyamohanty@gmail.com>
2026-05-09 14:12:34 +0530

    ..

--- beat_dir/scheduler.py
+++ beat_dir/scheduler.py
@@ -2,11 +2,15 @@ import os
 from .connection import beat_app
 from pushbullet import Pushbullet
 
+# @beat_app.task(name="beat_dir.scheduler.heartbeat_check")
+# def heartbeat_check():
+#     try:
+#         pb = Pushbullet(os.getenv("YOUR_PUSHBULLET_API_KEY"))
+#         pb.push_note("Pulse", "System is healthy!")
+#         # print("Success: Message sent to Pushbullet")
+#     except Exception as e:
+#         print(f"Error: {e}")
+
 @beat_app.task(name="beat_dir.scheduler.heartbeat_check")
 def heartbeat_check():
-    try:
-        pb = Pushbullet(os.getenv("YOUR_PUSHBULLET_API_KEY"))
-        pb.push_note("Pulse", "System is healthy!")
-        # print("Success: Message sent to Pushbullet")
-    except Exception as e:
-        print(f"Error: {e}")
+    pass
