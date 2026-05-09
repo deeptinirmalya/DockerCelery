850eb3e63780610d033df1ef8ca77a1a2099ded4
Deepti-web <deeptinirmalyamohanty@gmail.com>
2026-05-09 14:06:21 +0530

    ..

--- beat_dir/scheduler.py
+++ beat_dir/scheduler.py
@@ -7,6 +7,6 @@ def heartbeat_check():
     try:
         pb = Pushbullet(os.getenv("YOUR_PUSHBULLET_API_KEY"))
         pb.push_note("Pulse", "System is healthy!")
-        print("Success: Message sent to Pushbullet")
+        # print("Success: Message sent to Pushbullet")
     except Exception as e:
         print(f"Error: {e}")
